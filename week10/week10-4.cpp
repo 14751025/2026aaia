eaf1628967ba44524fe39a3c96db86a535e443ec
14751025 <14751025@me.mcu.edu.tw>
2026-04-27 11:35:31 +0800

    week10

---
+++ week10/week10-4.cpp
@@ -0,0 +1,20 @@
+///week10-4.cpp 交換兩瓶水
+#include <stdio.h>
+int main(){
+    ///黑色的酒vs100%的白開水
+    int a=9,b=100;///一開始
+    printf("a:%d b:%d\n",a,b);///印出來
+
+    ///交換一下
+    int temp=a;///臨時拿個瓶子temp
+    a=b; ///有點押韻
+    b=temp;
+
+
+
+    printf("a:%d b:%d\n",a,b);
+
+
+
+
+}
